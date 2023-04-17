ce9217624db82ba39424aa0ebe064fa48f726bb7
Tano <tantn1@unicloud.com.vn>
2023-04-17 10:27:23 +0700

    add config firebase

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,6 +1,6 @@
 const CONSTANTS = {
     // BASE_API: 'https://lovermoneyapi.somee.com/api',
-    BASE_API: 'https://localhost:44367/api',
+    BASE_API: 'https://tantanotnt96.bsite.net/api',
     TOKEN: 'token',
     USER_NAME: 'user name',
     USER_ID: 'user id'
